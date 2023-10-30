8c25e7c7b9de4c16f9bfb8f47874f57548dafef7
mathankumar.s <mathankumar.s@200411LTP2851>
2023-10-30 10:36:45 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bindings';
+  themeName:string="red";
+  size:number=100;
+  enableDisable:boolean=false;
+  firstName:string="Mathan";
+  lastName:string="Kumar";
+  reset:boolean=false;
+  
+
+
 }
